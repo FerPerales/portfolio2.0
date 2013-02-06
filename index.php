5aed578d8b90e75f74e2be0563f964f7edae0878
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 14:27:27 -0600

    Add favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Fer Perales :: Welcome</title>
+		<link rel="shortcut icon" href="img/favicon.ico">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="Fer Perales">
