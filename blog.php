37e52747feac7bb4df0dac0aa4cd0e75d18dabd1
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 15:05:01 -0600

    Add footer

--- blog.php
+++ blog.php
@@ -43,9 +43,12 @@
 		</div>
 		<div id="footer">
 			<div class="container">
-
+				<div class="row pull-right">
+					<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
+				</div>
 			</div>
 		</div>
+
 
 		<!-- Le javascript
 		================================================== -->
