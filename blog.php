7a25823c0f62c999f8461664deaec5a7ebbdad99
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 15:01:21 -0600

    Add blog scaffold

---
+++ blog.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Fer Perales :: Blog</title>
+		<link rel="shortcut icon" href="img/favicon.ico">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="Fer Perales">
+
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/footer.css" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<!-- Part 1: Wrap all page content here -->
+		<div id="wrap">
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">				
+					<div class="row pull-right">
+						<ul class="nav nav-pills">
+						  <li><a href="index.php">Home</a></li>
+						  <li><a href="resume.php">Résumé</a></li>
+						  <li class="active"><a href="portfolio.php">Portfolio</a></li>						  
+						  <li><a href="contact.php">Contact</a></li>
+						  <li><a href="http://blog.ferperales.net">Blog</a></li>
+						</ul>
+					</div>
+					<h1>Blog</h1>
+				</div>
+			</div>
+			<div id="push"></div>
+		</div>
+		<div id="footer">
+			<div class="container">
+
+			</div>
+		</div>
+
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
