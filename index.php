658d8e12fcc10f05c45eee32d81f5f84a2cae6df
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 12:59:14 -0600

    Eliminate unnecesary code in the footer

--- index.php
+++ index.php
@@ -49,14 +49,9 @@
 
 		<div id="footer">
 			<div class="container">
-
-					<div class="row">
-						<div class="pull-right">
-							<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
-						</div>
-					</div>
+				<div class="row pull-right">
+					<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
 				</div>
-
 			</div>
 		</div>
 
