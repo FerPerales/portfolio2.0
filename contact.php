0b84878d5f2eb7adaaf752b5a590c78243746004
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 14:55:31 -0600

    Add disqus form

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Fer Perales :: Welcome</title>
+		<link rel="shortcut icon" href="img/favicon.ico">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="Fer Perales">
+
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/footer.css" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<!-- Part 1: Wrap all page content here -->
+		<div id="wrap">
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">				
+					<div class="row pull-right">
+						<ul class="nav nav-pills">
+						  <li><a href="/">Home</a></li>
+						  <li><a href="resume.php">Résumé</a></li>
+						  <li><a href="portfolio.php">Portfolio</a></li>						  
+						  <li class="active"><a href="contact.php">Contact</a></li>
+						  <li><a href="http://blog.ferperales.net">Blog</a></li>
+						</ul>
+					</div>
+					<h1>Contact</h1>
+				</div>
+    <div id="disqus_thread"></div>
+    
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    
+			</div>
+			<div id="push"></div>
+		</div>
+		<div id="footer">
+			<div class="container">
+				<div class="row pull-right">
+					<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
+				</div>
+			</div>
+		</div>
+
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script type="text/javascript">
+	        var disqus_shortname = 'comentariosferperalesnet';
+	        (function() {
+	            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+	            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	        })();
+	    </script>
+
+	</body>
+</html>
