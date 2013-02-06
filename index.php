fe30967a8c257ff70848a8fd603d9ee359edb23f
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 12:38:53 -0600

    Add about.me apps into footer

--- index.php
+++ index.php
@@ -39,6 +39,13 @@
 		<div id="footer">
 			<div class="container">
 
+					<div class="row">
+						<div class="pull-right">
+							<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 
