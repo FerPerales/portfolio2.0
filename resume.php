881642706f121e3472b9fe3204866fcee374f116
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 15:45:26 -0600

    Change title

--- resume.php
+++ resume.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Fer Perales :: Welcome</title>
+		<title>Fer Perales :: Résumé</title>
 		<link rel="shortcut icon" href="img/favicon.ico">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
