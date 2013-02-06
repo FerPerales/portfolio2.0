7fbb1f4a5a24b431e23a7421819640b353c09b94
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 13:52:14 -0600

    Add scaffold

---
+++ resume.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Fer Perales :: Welcome</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="Fer Perales">
+
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/footer.css" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<!-- Part 1: Wrap all page content here -->
+		<div id="wrap">
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">				
+					<div class="row pull-right">
+						<ul class="nav nav-pills">
+						  <li><a href="index.php">Home</a></li>
+						  <li class="active"><a href="resume.php">Résumé</a></li>
+						  <li><a href="portfolio.php">Portfolio</a></li>						  
+						  <li><a href="contact.php">Contact</a></li>
+						  <li><a href="http://blog.ferperales.net">Blog</a></li>
+						</ul>
+					</div>
+					<h1>Résumé</h1>
+				</div>
+				<div class="row">
+					<div class="pull-right">Download <a href="cv/"><img src="img/pdf_icon.gif" alt="" /></a></div>
+				</div>
+				<div class="row">
+					<div class="span8 offset2">
+						<div class="row">
+						<div class="span4 pull-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est </div>
+						<div class="span4 pull-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="push"></div>
+		</div>
+		<div id="footer">
+			<div class="container">
+				<div class="row pull-right">
+					<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
+				</div>
+			</div>
+		</div>
+
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
