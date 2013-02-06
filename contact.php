feba9944d38bf2752c9788cf24c91ae4ebe719a2
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 15:11:12 -0600

    Make main column narrower

--- contact.php
+++ contact.php
@@ -26,23 +26,36 @@
 
 			<!-- Begin page content -->
 			<div class="container">
-				<div class="page-header">				
+				<div class="page-header">
 					<div class="row pull-right">
 						<ul class="nav nav-pills">
-						  <li><a href="index.php">Home</a></li>
-						  <li><a href="resume.php">Résumé</a></li>
-						  <li><a href="portfolio.php">Portfolio</a></li>						  
-						  <li class="active"><a href="contact.php">Contact</a></li>
-						  <li><a href="blog.php">Blog</a></li>
+							<li>
+								<a href="index.php">Home</a>
+							</li>
+							<li>
+								<a href="resume.php">Résumé</a>
+							</li>
+							<li>
+								<a href="portfolio.php">Portfolio</a>
+							</li>
+							<li class="active">
+								<a href="contact.php">Contact</a>
+							</li>
+							<li>
+								<a href="blog.php">Blog</a>
+							</li>
 						</ul>
 					</div>
 					<h1>Contact</h1>
 				</div>
-    <div id="disqus_thread"></div>
-    
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-    
+				
+				<div class="span8 offset2">
+					<div id="disqus_thread"></div>
+					<noscript>
+						Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+					</noscript>
+					<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+				</div>
 			</div>
 			<div id="push"></div>
 		</div>
@@ -60,13 +73,15 @@
 		<script src="js/jquery-1.9.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script type="text/javascript">
-	        var disqus_shortname = 'comentariosferperalesnet';
-	        (function() {
-	            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-	            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	        })();
-	    </script>
+			var disqus_shortname = 'comentariosferperalesnet';
+			(function() {
+				var dsq = document.createElement('script');
+				dsq.type = 'text/javascript';
+				dsq.async = true;
+				dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			})();
+		</script>
 
 	</body>
 </html>
