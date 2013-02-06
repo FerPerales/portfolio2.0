2c66ff786699687cd5233cd7dbadbdbefdac5542
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-06 12:19:18 -0600

    Create index scaffold

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Fer Perales :: Welcome</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="Fer Perales">
+
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/footer.css" rel="stylesheet">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		<!-- Part 1: Wrap all page content here -->
+		<div id="wrap">
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">
+					<h1>Sticky footer</h1>
+				</div>
+				<p class="lead">
+					Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.
+				</p>
+			</div>
+
+			<div id="push"></div>
+		</div>
+
+		<div id="footer">
+			<div class="container">
+
+			</div>
+		</div>
+
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
