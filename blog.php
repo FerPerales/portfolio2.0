9c344d7ea9feddd943be668c31368892f1b8886c
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-17 15:39:47 -0600

    Add script links

--- blog.php
+++ blog.php
@@ -75,8 +75,10 @@
 		<!-- Le javascript
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="http://www.google.com/jsapi"></script>
 		<script src="js/jquery-1.9.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.gfeed.js"></script>
 
 	</body>
 </html>
