8f3dcfc3dd9cef01d37f842f54ce66575fec102f
Fer Perales <fer.perales.montoya@gmail.com>
2013-02-24 13:08:45 -0600

    Add footer.php file

--- contact.php
+++ contact.php
@@ -61,13 +61,10 @@
 			</div>
 			<div id="push"></div>
 		</div>
-		<div id="footer">
-			<div class="container">
-				<div class="row pull-right">
-					<script src="//about.me/embed/ferperales?image=0&amp;name=0&amp;headline=0&amp;bio=0&amp;style=site"></script>
-				</div>
-			</div>
-		</div>
+<?php	
+	require 'php/footer.php';
+?>
+
 
 		<!-- Le javascript
 		================================================== -->
