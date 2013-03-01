25194a4caa14915769120cdcf2f47ec7774f70c9
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 11:31:23 -0600

    Add debugger info

--- js/counter.js
+++ js/counter.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	/* This code is executed after the DOM has been completely loaded */
 
 	$('#times').click(function(){
-
+		console.log 'click!'
 		var countSpan = $('.download-count',this);
+		console.log countSpan
 		countSpan.text( parseInt(countSpan.text())+1);
+		
 	});
 });
