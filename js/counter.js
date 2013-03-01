4261847df66d0ab3ffe3df6fcee5a73e89886020
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 12:33:52 -0600

    Remove this reference

--- js/counter.js
+++ js/counter.js
@@ -2,7 +2,7 @@ console.log ("Counter is ready ");
 $(document).ready(function(){
 	$('#times').click(function(){
 		console.log ('click!');
-		var countSpan = $('span.download-count',this);
+		var countSpan = $('span.download-count');
 		console.log(countSpan)
 		console.log("Current counter: " + countSpan.text());
 		countSpan.text( parseInt(countSpan.text())+1);
