7d1c8e922bf70f13f5fbe9990efb613eea6ae44c
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 10:50:31 -0600

    Add h1

--- times.php
+++ times.php
@@ -1,6 +1,5 @@
 <?php
 	
 	$hit_count = @file_get_contents('count.txt');
-	echo 'File has been downloaded '.$hit_count.' times';
-
+	echo '<h1>File has been downloaded '.$hit_count.' times</h1>';
 ?>
