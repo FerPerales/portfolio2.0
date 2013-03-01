e92cda366b16c150652f9583a5f2aded4727825b
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 11:29:25 -0600

    Change download counter method

--- cv.php
+++ cv.php
@@ -2,5 +2,8 @@
 	$hit_count = @file_get_contents('count.txt');
 	$hit_count++;
 	@file_put_contents('count.txt', $hit_count);
-	header('Location: http://www.ferperales.net/docs/CV Fernando Perales_en.pdf'); // redirect to the real file to be downloaded
+	header('Content-disposition: attachment; filename=huge_document.pdf');
+	header('Content-type: application/pdf');
+	readfile('docs/CV Fernando Perales_en.pdf');
+	//header('Location: http://www.ferperales.net/docs/CV Fernando Perales_en.pdf'); // redirect to the real file to be downloaded
 ?>
