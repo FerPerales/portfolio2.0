5ced67d7d7d421bb824b0785240cfea45c0d7889
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 12:26:19 -0600

    Add span to jquery selector

--- js/counter.js
+++ js/counter.js
@@ -2,7 +2,7 @@ console.log ("Counter is ready ");
 $(document).ready(function(){
 	$('#times').click(function(){
 		console.log ('click!');
-		var countSpan = $('.download-count',this);
+		var countSpan = $('span.download-count',this);
 		console.log(countSpan)
 		console.log("Current counter: " + countSpan.text());
 		countSpan.text( parseInt(countSpan.text())+1);
