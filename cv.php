f6d644dc9316ef31fafb9a486fba7427818aa837
Fer Perales <fer.perales.montoya@gmail.com>
2013-03-01 11:41:46 -0600

    Fix download file name

--- cv.php
+++ cv.php
@@ -2,8 +2,7 @@
 	$hit_count = @file_get_contents('count.txt');
 	$hit_count++;
 	@file_put_contents('count.txt', $hit_count);
-	header('Content-disposition: attachment; filename=huge_document.pdf');
+	header('Content-disposition: attachment; filename=CV Fernando Perales_en.pdf');
 	header('Content-type: application/pdf');
 	readfile('docs/CV Fernando Perales_en.pdf');
-	//header('Location: http://www.ferperales.net/docs/CV Fernando Perales_en.pdf'); // redirect to the real file to be downloaded
 ?>
