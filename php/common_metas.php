f1c5b1fa606f5bd1989ad5462555de6b35e342e1
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-14 00:32:22 -0500

    Add return between each line

--- php/common_metas.php
+++ php/common_metas.php
@@ -1,13 +1,12 @@
 <?php
-		print '<link rel="shortcut icon" href="img/favicon.ico">';
-		print '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-		print '<meta name="description" content="">';
-		print '<meta name="author" content="Fer Perales">';
+		print '<link rel="shortcut icon" href="img/favicon.ico">'."\n";
+		print '<meta name="viewport" content="width=device-width, initial-scale=1.0">'."\n";
+		print '<meta name="description" content="">'."\n";
+		print '<meta name="author" content="Fer Perales">'."\n";
 		
-		print '<meta property="og:url" content="http://ferperales.net"/>';
-		print '<meta property="og:image" content="http://ferperales.net/img/favicon.ico"/>';
-		print '<meta property="og:type" content="website"/>';
-		print '<meta property="og:site_name" content="Fer Perales"/>';
-		print '<meta property="og:description" content="Fer Perales is a software craftsman apretience"/>';
-		
+		print '<meta property="og:url" content="http://ferperales.net"">'."\n";
+		print '<meta property="og:image" content="http://ferperales.net/img/favicon.ico"">'."\n";
+		print '<meta property="og:type" content="website"">'."\n";
+		print '<meta property="og:site_name" content="Fer Perales"">'."\n";
+		print '<meta property="og:description" content="Fer Perales is a software craftsman apretience"">'."\n";
 ?>
