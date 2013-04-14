7debb8f0dbc330344e25e5654f4b98cec277431a
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-14 00:12:53 -0500

    Extract common meta tags

---
+++ php/common_metas.php
@@ -0,0 +1,13 @@
+<?php
+		echo '<link rel="shortcut icon" href="img/favicon.ico">';
+		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+		echo '<meta name="description" content="">';
+		echo '<meta name="author" content="Fer Perales">';
+		
+		echo '<meta property="og:url" content="http://ferperales.net"/>';
+		echo '<meta property="og:image" content="http://ferperales.net/img/favicon.ico"/>';
+		echo '<meta property="og:type" content="website"/>';
+		echo '<meta property="og:site_name" content="Fer Perales"/>';
+		echo '<meta property="og:description" content="Fer Perales is a software craftsman apretience"/>';
+		
+?>
