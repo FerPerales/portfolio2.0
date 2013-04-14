905c23ea65d6ea08aa7de12f58f0fc04b82895a0
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-14 00:29:01 -0500

    Replace echo for print to keep identation

--- php/common_metas.php
+++ php/common_metas.php
@@ -1,13 +1,13 @@
 <?php
-		echo '<link rel="shortcut icon" href="img/favicon.ico">';
-		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-		echo '<meta name="description" content="">';
-		echo '<meta name="author" content="Fer Perales">';
+		print '<link rel="shortcut icon" href="img/favicon.ico">';
+		print '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+		print '<meta name="description" content="">';
+		print '<meta name="author" content="Fer Perales">';
 		
-		echo '<meta property="og:url" content="http://ferperales.net"/>';
-		echo '<meta property="og:image" content="http://ferperales.net/img/favicon.ico"/>';
-		echo '<meta property="og:type" content="website"/>';
-		echo '<meta property="og:site_name" content="Fer Perales"/>';
-		echo '<meta property="og:description" content="Fer Perales is a software craftsman apretience"/>';
+		print '<meta property="og:url" content="http://ferperales.net"/>';
+		print '<meta property="og:image" content="http://ferperales.net/img/favicon.ico"/>';
+		print '<meta property="og:type" content="website"/>';
+		print '<meta property="og:site_name" content="Fer Perales"/>';
+		print '<meta property="og:description" content="Fer Perales is a software craftsman apretience"/>';
 		
 ?>
