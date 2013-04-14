effe4bd507cb961b51a617872868cbc7a1058aab
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-14 00:18:37 -0500

    Add dynamic styles to breadcrumb

--- portfolio.php
+++ portfolio.php
@@ -211,21 +211,20 @@
 			var projects = $('.thumbnails');
 			var language = "all";
 			var prev = language;
+			var prevOption = $('li[data-language*="all"]');
 			$('.breadcrumb').on('click', 'li', function(){	
-							
-							
-				language = $(this).data("language");
-				
+				language = $(this).data("language");				
 				if (language == 'all') {
 					projects.find('li').show(1000);
 				}	else {					
-					language = $(this).data("language");					
+					language = $(this).data("language");	
 					projects.find('li[data-language*="' + language + '"]').show(1000);
 					projects.find('li:not([data-language*="' + language + '"])').hide(1000);
-				}
+				}				
+				prevOption.removeClass('active');				
+				prevOption = $(this);
+				prevOption.addClass('active');
 			});
-			
-			
 		</script>
 		
 
