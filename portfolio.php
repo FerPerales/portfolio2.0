653a4e8ec1126f0c5297b9d29edf06f09459991e
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-30 21:37:52 -0500

    Add new menu

--- portfolio.php
+++ portfolio.php
@@ -36,23 +36,7 @@
 			<div class="container">
 				<div class="page-header">				
 					<div class="row pull-right">
-						<ul class="nav nav-pills">
-							<li id="index">
-								<a href="index">Home</a>
-							</li>
-							<li id='resume'>
-								<a href="resume">Résumé</a>
-							</li>
-							<li id='portfolio'>
-								<a href="portfolio">Portfolio</a>
-							</li>
-							<li id='contact'>
-								<a href="contact">Contact</a>
-							</li>
-							<li id='myblog'>
-								<a href="myblog">Blog</a>
-							</li>
-						</ul>
+<?php include_once("php/menu.php") ?>
 					<h1>Portfolio</h1>
 				</div>
 				
