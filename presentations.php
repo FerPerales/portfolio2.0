bcf5180644d61c96c31df60d51cc83015dbab441
Fer Perales <fer.perales.montoya@gmail.com>
2013-04-30 21:29:23 -0500

    Add presentations page

---
+++ presentations.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Fer Perales :: Résumé</title>
+		
+<?php	
+	require 'php/common_metas.php';
+?>		
+		<meta property="og:title" content="Fer Perales :: Resume"/>
+
+		<!-- CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/footer.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet"/>
+		<link rel="stylesheet" href="css/font-awesome-social.css" />
+		<link rel="stylesheet" href="css/font-awesome-ext.css" />
+		<link rel="stylesheet" href="css/style.css" />
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+<?php include_once("php/tracking.php") ?>
+
+<div id="wrap">
+
+			<!-- Begin page content -->
+			<div class="container">
+				<div class="page-header">
+					<div class="row pull-right">
+						<ul class="nav nav-pills">
+							<li id="index">
+								<a href="index">Home</a>
+							</li>
+							<li id='resume'>
+								<a href="resume">Résumé</a>
+							</li>
+							<li id='portfolio'>
+								<a href="portfolio">Portfolio</a>
+							</li>
+							<li id='contact'>
+								<a href="contact">Contact</a>
+							</li>
+							<li id='myblog'>
+								<a href="myblog">Blog</a>
+							</li>
+						</ul>
+					</div>
+					<h1>Presentations</h1>
+				</div>
+				<div class="span8 offset2">
+					<div>
+						One of the things I really love is to share the little knowledge I've gotten along these years and the best 
+					</div>
+					
+				</div>
+			</div>
+			<div id="push"></div>
+		</div>
+
+
+
+
+<?php	
+	require 'php/footer.php';
+?>
+
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-1.9.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/select_menu.js"></script>
+		<script src="js/counter.js"></script>
+	</body>
