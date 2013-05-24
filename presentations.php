192475afab28127d7e822df5ce431c7d38d3b7b8
Fer Perales <fer.perales.montoya@gmail.com>
2013-05-23 23:59:06 -0500

    Update presentations page

--- presentations.php
+++ presentations.php
@@ -32,9 +32,23 @@
 				<h1>Presentations</h1>
 			</div>
 			<div class="span8 offset2">
+				<p>I love giving talks about programming, startups, professional carreer and technology in general and I feel comfortable speaking in either Spanish or English</p>
+				<p>I have also given workshops about mobile development, ruby, ruby on rails and javascript. Those workshops lasted an average of three hours and I always try to make them as engaging as possible.</p>
+				<p>
+					You can take a look at my <a href="https://speakerdeck.com/ferperales">SpeakerDeck</a> profile and my <a href="http://prezi.com/user/FerPerales/">Prezi</a> profile. I would really appreciate your feedback in case you attended one of my talks by sharing your thoughts on my <a href="http://speakerrate.com/ferperales">SpeakerRate</a> profile.
+				</p>					
+				<p>I'm always willing to give talks and workshops at no cost and, if you consired that I can give a talk that would be useful for you or your organization, get in touch with <a href="contact">me</a></p>
+
 				<div>
-					Under construction. You can take a look at my <a href="https://speakerdeck.com/ferperales">SpeakerDeck</a> profile and my <a href="http://prezi.com/user/FerPerales/">Prezi</a> profile. I would really appreciate your feedback in case you attended one of my talks by sharing your thoughts on my <a href="http://speakerrate.com/ferperales">SpeakerRate</a> profile.
-				</div>					
+					<h2>Latest talk</h2>
+					<p>I presented a talk called 'I finished university school: now what I do?' at <a href="hpp://divecfest.cucei.udg.mx">DivecFest</a> 2013 in May 14th. In this talk, I expose the opportunities that a just graduated IT student have to start a sucessful professional carrer: what kind of jobs can their get as well as the advantages and disadvantages of those options and some tips and advise to help them get their dream job </p>
+					<p>The talk was given in Spanish and the slides used are showed below</p>
+					
+					
+					
+					<script async class="speakerdeck-embed" data-id="a93b60209e7501301b2f469a61e096c5" data-ratio="1.33507170795306" src="//speakerdeck.com/assets/embed.js"></script>
+				</div>
+
 			</div>
 		</div>
 		<div id="push"></div>
